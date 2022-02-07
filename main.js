cf56c77dc1b175d0273c905f601595431c6cabb8
pcatana <catanapetru@gmail.com>
2022-02-07 15:01:22 +0000

    macos config

--- main.js
+++ main.js
@@ -52,9 +52,10 @@ function createWindow() {
 }
 
 if (isDev) {
-    require('electron-reload')(__dirname, {
-        electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
-    })
+    if (!process.platform === 'darwin')
+        require('electron-reload')(__dirname, {
+            electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+        })
 }
 
 if (process.platform === 'darwin') {
