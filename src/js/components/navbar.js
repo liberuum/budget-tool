2409a0fb9868babd023998f1e125e836154eb4b4
pcatana <catanapetru@gmail.com>
2022-10-19 18:01:38 +0100

    added test text when user is using dev env

--- src/js/components/navbar.js
+++ src/js/components/navbar.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Flex, Heading, Button, Divider } from "theme-ui"
 import { Link, useNavigate } from 'react-router-dom'
 
 export default function Navbar() {
+    const [isDev, setIsDev] = useState(false);
+
+    useEffect(async () => {
+        const dev = await electron.getIsDev();
+        setIsDev(dev)
+    })
+
     const navigate = useNavigate();
     return (
         <Container>
-            <Heading>MakerDAO Budget Tool</Heading>
+            {isDev ? <Heading>MakerDAO Budget Tool <span style={{color: 'red'}}>Test</span> </Heading> : <Heading>MakerDAO Budget Tool</Heading>}
             <Divider></Divider>
             <Flex variant="flex.header">
                 <Flex as="nav">
-                    <Button onClick={() => navigate('/')} sx={{mx: 1}}>Budget Sheets</Button>
+                    <Button onClick={() => navigate('/')} sx={{ mx: 1 }}>Budget Sheets</Button>
                     <Button onClick={() => navigate('/settings')}>Settings</Button>
                 </Flex>
             </Flex>
